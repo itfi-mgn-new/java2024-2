b9831ad711244e2e4211aeeb6be8bdeb36849fae
Student <Student@IMAC-201-101-PC.itf.local>
2024-05-15 19:48:13 +0500

    Daily commiyt

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -1,10 +1,40 @@
 package lesson1;
 
-public class HelloWorld {
+import java.lang.*;
+import lesson1.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.err.println("sdsds");
+public class HelloWorld /*extends java.lang.Object*/ {
+	static int x = 0, z, t = -1;
+	int y = 10;
+	
+	
+	public static void main(java.lang.String[] args) {
+		byte	b1 = 10, b2 = 20, b3 = (byte)(b1 + b2);
+	
+		System.err.println("b1+b2="+(b1+b2));
+		System.err.println("b1-b2="+(b1-b2));
+		System.err.println("b1*b2="+(b1*b2));
+		System.err.println("b1/b2="+(b1/b2));
+		System.err.println("b1%b2="+(b1%b2));
+	
+		System.err.println("b1|b2="+(b1|b2));
+		System.err.println("b1&b2="+(b1&b2));
+		System.err.println("b1^b2="+(b1^b2));
+		System.err.println("~b1="+(~b1));
+		
+		System.err.println("b1<<1="+(b1<<1));
+		System.err.println("b1>>1="+(b1>>1));
+		System.err.println("b1>>>1="+(b1>>>1));
+
+		System.err.println("(-b1)>>1="+((-b1)>>1));
+		System.err.println("(-b1)>>>1="+((-b1)>>>1));
+	
+		System.err.println("b1==b2="+(b1==b2));
+		System.err.println("b1>b2="+(b1>b2));
+		System.err.println("b1>=b2="+(b1>=b2));
+		System.err.println("b1<b2="+(b1<b2));
+		System.err.println("b1<=b2="+(b1<=b2));
+		System.err.println("b1!=b2="+(b1!=b2));
 	}
 
 }
